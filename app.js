45a22bf15f91df134deef6e55cd382fe1589a9ab
github username <github email address>
2020-06-15 15:11:12 -0600

    Continuing notes for regex practice. Adding the OR method, i flag, and .match()

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 // Using the .test() method
 let myString = "Hello, aliens!";
-let myRegex = /aliens/;
-let result = myRegex.test(myString);
-
+let testing = /aliens/;
+let result = testing.test(myString);
 // console.log(result)
-// result = true
 
 // Match Literal Strings
 let story = "Somewhere JJ is hiding in this text. We don't need to find jj or Jj.";
 let findCharacter = /JJ/;
 let result2 = findCharacter.test(story);
-
 // console.log(result2)
-// result = true
+
+// Match a Literal String with Different Possibilities
+let jj = "JJ is a dog.";
+let pet = /dog|cat|bird|fish/;
+let result3 = pet.test(jj);
+// console.log(result3)
+
+// Ignore Case While Matching
+let string = "I will Change for CHANGE, and not just change.";
+let findAllCase = /change/i;
+let result4 = findAllCase.test(string);
+// console.log(result4)
+
+// Extract Matches
+let extractStr = "Hello, aliens";
+let codingRegex = /aliens/; 
+let result5 = extractStr.match(codingRegex); 
+// console.log(result5)
