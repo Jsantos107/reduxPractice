25b730156f311e50edf8ec12f9dff3a279e46594
github username <github email address>
2020-06-15 16:33:57 -0600

    Continuing notes for regex practice. Adding the g flag, wildcard character, and [] brackets.

--- app.js
+++ app.js
@@ -24,6 +24,26 @@ let result4 = findAllCase.test(string);
 
 // Extract Matches
 let extractStr = "Hello, aliens";
-let codingRegex = /aliens/; 
-let result5 = extractStr.match(codingRegex); 
+let codingRegex = /aliens/;
+let result5 = extractStr.match(codingRegex);
 // console.log(result5)
+
+//Find More Than the First Match
+let story2 = "Somewhere JJ is hiding in this text. We also need to find jj and Jj.";
+let findRegex = /jj/ig;
+let result6 = story2.match(findRegex);
+// console.log(result6)
+
+// Match Anything with Wildcard Period
+let huntStr = "Lets hunt for some pizza";
+let hugStr = "I'll hug you";
+let huRegex = /hu./;
+let result7 = huRegex.test(huntStr);
+let result8 =huRegex.test(hugStr);
+// console.log(result7, result8)
+
+// Match Single Character with Multiple Possibilities
+let quoteSample = "Oooooooooookay, we are going to find all vowels in this sentence EXCEPT for 'o'.";
+let vowelRegex = /[aeiu]/gi; // Change this line
+let result9 = quoteSample.match(vowelRegex); // Change this line
+// console.log(result9)
